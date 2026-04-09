58efa5a7f9afc2202cef5a34dbbc0cd74207a5d9
Dmytro Ivanenko <ivanenko2010@gmail.com>
2026-04-09 08:04:32 +0300

    js update

--- js/effects/imageLoad.js
+++ js/effects/imageLoad.js
@@ -1,5 +1,69 @@
-const imageUrls = [
+// const imageUrls = [
     
+// 'web.mp4',
+// 'face.mp4',
+// 'cleaner.mp4',
+// 'tesla.mp4',
+// 'bike.mp4',
+// 'coca_colla.mp4',
+// 'bisnod.mp4',
+// 'ducatti.mp4',
+// 'pottrey.mp4',
+// 'sushi.mp4',
+// 'asint.mp4',
+// 'fly.gif',
+// 'banana.mp4',
+// '180.mp4',
+// 'bass.mp4',
+// 'good.mp4',
+// 'stul.mp4',
+// 'sull2.mp4',
+// 'caddi.gif',
+// 'sketch.mp4',
+// 'remco.mp4',
+// 'sushi.mp4',
+// 'surfing.mp4',
+// 'david.mp4'
+    
+//     ];
+
+// function loadImage(url) {
+//   return new Promise((resolve, reject) => {
+//     const img = new Image();
+//     img.onload = () => resolve(img);
+//     img.onerror = (err) => reject(err);
+//     img.src = url;
+//   });
+// }
+
+// async function loadSequentially() {
+//   for (const url of imageUrls) {
+//     try {
+//       const img = await loadImage(url);
+//       document.body.appendChild(img);
+//       console.log(`Loaded: ${url}`);
+//     } catch (err) {
+//       console.error(`Failed: ${url}`, err);
+//     }
+//   }
+// }
+
+// loadSequentially();
+
+
+function loadImagesInSequence(images) {
+  if (!images.length) {
+    return;
+  }
+
+  var img = new Image(),
+      url = images.shift();
+
+  img.onload = function(){ loadImagesInSequence(images) };
+  img.src = url;
+}
+
+loadImagesInSequence([
 'web.mp4',
 'face.mp4',
 'cleaner.mp4',
@@ -24,28 +88,4 @@ const imageUrls = [
 'sushi.mp4',
 'surfing.mp4',
 'david.mp4'
-    
-    ];
-
-function loadImage(url) {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = (err) => reject(err);
-    img.src = url;
-  });
-}
-
-async function loadSequentially() {
-  for (const url of imageUrls) {
-    try {
-      const img = await loadImage(url);
-      document.body.appendChild(img);
-      console.log(`Loaded: ${url}`);
-    } catch (err) {
-      console.error(`Failed: ${url}`, err);
-    }
-  }
-}
-
-loadSequentially();
+]);
